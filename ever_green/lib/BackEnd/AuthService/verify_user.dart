2503a55dde86f20046f357e3b80b43975d9ac175
Projet2CPE <126556371+Projet2CPE@users.noreply.github.com>
2023-05-16 20:12:55 +0100

    Update verify_user.dart
    comment

--- ever_green/lib/BackEnd/AuthService/verify_user.dart
+++ ever_green/lib/BackEnd/AuthService/verify_user.dart
@@ -7,7 +7,7 @@ import 'package:uuid/uuid.dart';
 import '../../main.dart';
 
 
-
+// Class pour vérifier l'utilisateur en cas d'inscription
 class VerifyUserEmail extends StatefulWidget {
   const VerifyUserEmail({Key? key}) : super(key: key);
   @override
@@ -15,20 +15,20 @@ class VerifyUserEmail extends StatefulWidget {
 }
 
 class _VerifyUserEmailState extends State<VerifyUserEmail> {
-  late User user;
-  late Timer timer;
+  late User user; // l'utilisateur qui vient d'essayer de s'enregistrer
+  late Timer timer; // la minuterie utilisée pour vérifier si l'utilisateur a vérifié son e-mail dans un intervalle de temps donné
   @override
   void initState() {
-    user = FirebaseAuth.instance.currentUser!;
-    user.sendEmailVerification();
+    user = FirebaseAuth.instance.currentUser!; // obtenir l'utilisateur à partir des données de firebase
+    user.sendEmailVerification(); // envoi d'un e-mail de vérification à l'adresse e-mail de l'utilisateur
     timer = Timer.periodic(const Duration(seconds: 3), (timer) {
-      verifyUser();
+      verifyUser(); // toutes les 3 secondes, l'apk vérifie si l'utilisateur a vérifié son e-mail ou non
     });
     super.initState();
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { // le UI de la page du vérification
     return Container(
       decoration: const BoxDecoration(
         color: Colors.greenAccent,
@@ -52,17 +52,17 @@ class _VerifyUserEmailState extends State<VerifyUserEmail> {
     super.dispose();
   }
 
-  Future verifyUser() async {
+  Future verifyUser() async { // Fonction qui permet de verifier si le user a verifier son email
     Parent methodParent = Parent([], 0, const Uuid().v4().toString());
     try {
-      User user = FirebaseAuth.instance.currentUser!;
+      User user = FirebaseAuth.instance.currentUser!; // on récuper le useer
       await user.reload();
-      if (user.emailVerified) {
-        timer.cancel();
-        methodParent.createData(user.uid);
-        await navigatorKey.currentState!.pushReplacementNamed('/ChildSelector');
+      if (user.emailVerified) { // si le user a vérifer son email 
+        timer.cancel(); // on arréte la  minuterie
+        methodParent.createData(user.uid); // on crée de nouvelles données pour l'utilisateur 
+        await navigatorKey.currentState!.pushReplacementNamed('/ChildSelector'); // Redirection vers la page '/ChildSelector' en remplaçant la route actuelle
       }
-    } on FirebaseAuthException catch (e) {
+    } on FirebaseAuthException catch (e) { // si une erreur s'est produite, elle est imprimée sur le terminal
       print(e);
     }
   }
